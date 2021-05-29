208756d25ab3bedb37cde468853aa9477ea444db
jfulop99 <janos.fulop.mail@gmail.com>
2021-05-29 15:47:01 +0200

    BirdWatcher with JDBCTemplate

--- src/main/java/hu/nive/ujratervezes/birdwatcher/BirdWatcher.java
+++ src/main/java/hu/nive/ujratervezes/birdwatcher/BirdWatcher.java
@@ -1,5 +1,10 @@
 package hu.nive.ujratervezes.birdwatcher;
 
+// import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -16,7 +21,7 @@ public class BirdWatcher {
         this.dataSource = dataSource;
     }
 
-    public List<String> getUniqueBirds() {
+    public List<String> getUniqueBirdsJDBC() {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement ps = conn.prepareStatement("SELECT DISTINCT species FROM birds_spotted ORDER BY species")) {
             return getResults(ps);
@@ -34,5 +39,14 @@ public class BirdWatcher {
             }
         }
         return result;
+    }
+
+
+    public List<String> getUniqueBirds() {
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate.query("SELECT DISTINCT species FROM birds_spotted ORDER BY species",
+                (resultSet, i) -> resultSet.getString("species"));
+
     }
 }
